1bd5d4af6e2b504472d6a6c0e415b6950bcd7e93
Cyril Cartoux <6ssou@protonmail.com>
2020-07-16 14:59:36 +0200

    build for prod

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/', express.static(path.join(__dirname, 'angular')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
@@ -26,5 +27,8 @@ app.use((req, res, next) => {
 
 app.use(postRoutes)
 app.use(authRoutes)
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'angular', 'index.html'));
+})
 
 module.exports = app;
